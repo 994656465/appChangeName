78ff7a528dcbf353e2209c980f3fe66709698019
cuijin <cuijin@zhipig.cn>
2024-02-27 15:44:08 +0800

    丰富其他图标

--- changeIcon/ViewController.m
+++ changeIcon/ViewController.m
@@ -13,47 +13,80 @@
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     UIButton * changeButton  = [UIButton buttonWithType:UIButtonTypeCustom];
     changeButton.frame = CGRectMake(100, 200, 100, 100);
-    [changeButton setTitle:@"切换图标" forState:UIControlStateNormal];
+    [changeButton setTitle:@"切换支付宝" forState:UIControlStateNormal];
     [changeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     changeButton.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.3];
-    [changeButton addTarget:self action:@selector(changebuttonClick) forControlEvents:UIControlEventTouchUpInside];
+    [changeButton addTarget:self action:@selector(changeZFBClick) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:changeButton];
+    
+    UIButton * twoButton  = [UIButton buttonWithType:UIButtonTypeCustom];
+    twoButton.frame = CGRectMake(250, 200, 100, 100);
+    [twoButton setTitle:@"智猪" forState:UIControlStateNormal];
+    [twoButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    twoButton.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.3];
+    [twoButton addTarget:self action:@selector(changeTwoClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:twoButton];
+    
+    
+    UIButton *threeButton  = [UIButton buttonWithType:UIButtonTypeCustom];
+    threeButton.frame = CGRectMake(100, 350, 100, 100);
+    [threeButton setTitle:@"基因" forState:UIControlStateNormal];
+    [threeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    threeButton.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.3];
+    [threeButton addTarget:self action:@selector(changeThreeClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:threeButton];
+    
+    UIButton *oriButton  = [UIButton buttonWithType:UIButtonTypeCustom];
+    oriButton.frame = CGRectMake(100, 500, 100, 100);
+    [oriButton setTitle:@"还原" forState:UIControlStateNormal];
+    [oriButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    oriButton.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.3];
+    [oriButton addTarget:self action:@selector(changeoriClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:oriButton];
+    
+    
 }
 
--(void)changebuttonClick{
-    // 判断可以更换
-    if ([[UIApplication sharedApplication] supportsAlternateIcons]) {
+-(void)changeZFBClick{
 
-         NSString *logoName = [[UIApplication sharedApplication] alternateIconName];
-        if (logoName) {
-            //         logoName 有值 设置nil ,切换默认图标
-            
-            [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
-                if (error) {
-                    NSLog(@"更换失败%@",error);
-                }else{
-                    NSLog(@"更换成功");
+    [self changeAppIconWithName:@"zfb"];
+}
+-(void)changeTwoClick{
 
-                }
-            }];
-        }else{
-//         logoName = nil 进行更换图标
-            [[UIApplication sharedApplication] setAlternateIconName:@"zfb" completionHandler:^(NSError * _Nullable error) {
-                if (error) {
-                    NSLog(@"更换失败%@",error);
-                }else{
-                    NSLog(@"更换成功");
-                }
-            }];
-        }
+    [self changeAppIconWithName:@"zhizhu"];
+}
+-(void)changeThreeClick{
+
+    [self changeAppIconWithName:@"jiyin"];
+
+}
+-(void)changeoriClick{
+
+    [self changeAppIconWithName:@""];
+
+}
+
+- (void)changeAppIconWithName:(NSString *)iconName {
+    if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+        return;
     }
+    
+    if ([iconName isEqualToString:@""]) {
+        iconName = nil;
+    }
+    [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"更换app图标发生错误了 ： %@",error);
+        }
+    }];
 }
+
+
 
 @end
